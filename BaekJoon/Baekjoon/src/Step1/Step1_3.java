3b60ccbcdc1b018ea7c5f67642fd8c959fd18229
skarns23 <skarns23@naver.com>
2022-01-04 18:26:40 +0900

    1단계 풀이

---
+++ BaekJoon/Baekjoon/src/Step1/Step1_3.java
@@ -0,0 +1,14 @@
+package Step1;
+
+public class Step1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
---
+++ BaekJoon/Baekjoon/src/Step1/Step1_5.java
@@ -0,0 +1,14 @@
+package Step1;
+import java.util.Scanner;
+public class Step1_5 {
+
+	public static void main(String[] args) {
+		int A,B;
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+		double result = (double)A/(double)B;
+		System.out.println(result);
+	}
+
+}
